117db10b35107dcd99301d662224e950be089462
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-11-16 22:16:20 +0530

    Front-end setup completed

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,9 +1,16 @@
-export default function App() {
+import "./App.css";
+
+
+function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        College Management System Frontend
-      </h1>
-    </div>
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">
+         hello world!
+        </h1>
+      </div>
+    </>
   );
 }
+
+export default App;
